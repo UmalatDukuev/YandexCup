2cd9cfe7aa8008d48da3dc614501b235a45d3969
Umalat <dukuev037@mail.ru>
2024-11-02 22:38:24 +0300

    intern

---
+++ intern/soda.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	n, _ := strconv.Atoi(sc.Text())
+	a := make([]int, n, n)
+	sc.Scan()
+	elements := strings.Split(sc.Text(), " ")
+	for i, value := range elements {
+		a[i], _ = strconv.Atoi(value)
+	}
+}
